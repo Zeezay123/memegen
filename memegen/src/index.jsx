66aaf27e3cf6eacb567db211e8d63e1ac093604e
azeez <aolam.ao@gmail.com>
2024-02-26 12:44:30 +0100

    memegen first commit

---
+++ memegen/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './style.css'
+
+
+function Page () {
+    return(
+        <App />
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Page/>)
